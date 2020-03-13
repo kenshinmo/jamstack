bcacb89cf88d47dd1a7277aad696639a58118cc9
Tae-Ho Kim <tae-ho.kim@gmx.de>
2020-03-13 19:37:37 +0100

    initial

---
+++ main.js
@@ -0,0 +1,23 @@
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner`
+  )
+    .then(res => res.json())
+    .catch(error => console.error(error));
+
+  const markup = repos
+    .map(
+      repo => `
+            <li>
+                <a href="${repo.html_url}">${repo.name}</a>
+                (🌟 ${repo.stargazers_count}) 
+            </li>
+            `
+    )
+    .join('');
+
+  const content = document.getElementById('content');
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos('Kenshinmo');
